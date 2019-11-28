239f030ad164911116fa1644b5056495ec53dd9b
agodot <zoldy.wf@gmail.com>
2019-11-27 21:43:09 -0700

    Some renaming...clobbered some things with naming so commits are gonna look weird

--- ComfortSpace.py
+++ ComfortSpace.py
@@ -1,22 +1,35 @@
+import pygame
+import pygame.midi as md
+from matplotlib import cm
 import numpy as np
 from scipy.spatial import distance
 import threading
-import pygame
-from matplotlib import cm
 
-import pygame.midi
+alt_colormap = cm.get_cmap('inferno', 100)
+colormap = cm.get_cmap('rainbow', 12)
+colors = [np.asarray(255 * np.asarray(colormap(c)[:-1])).astype('int') for c in range(12)]
 
+pygame.midi.init()
+screen = pygame.display.set_mode((800, 500))
+i = pygame.midi.Input(1)
+p_clock = pygame.time.Clock()
+player = pygame.midi.Output(0)
+
+current_instrument = 85 # 77 and 85, 90, 96, 113 -1 usually
+player.set_instrument(current_instrument) # 45
+
+notes = [False for n in range(128)]
+transmat = np.zeros((12, 12)).astype('float')
+
+N = 100
+points = np.random.random((N, 2)) # 100 points.
+point_notes = np.asarray([q%12 for q in range(N)])
 
 def sim():
 
     # This matrix will be based on the note relationships.
     # The note relationships will also be color coded.
     #multmat = (np.random.random((N,N))*0.2 + 0.8)
-    multmat = np.zeros((N,N))
-    for x in range(N):
-        for y in range(N):
-            multmat[x][y] = note_matrix[notes[x]][notes[y]]
-
     while True:
 
         #distances = distance.cdist(points, points, metric='chebyshev')
@@ -32,76 +45,89 @@ def sim():
         x_differences = (np.asarray(x_tiled > x_repeated).astype('int')-0.5)*2
         y_differences = (np.asarray(y_tiled > y_repeated).astype('int')-0.5)*2
 
+        # Make the multmat using the transmat...
+        multmat = np.ones((N,N)) - np.identity(N)*0.5
+        for x in range(N):
+            for y in range(N):
+                multmat[x][y] = 1 + transmat[point_notes[x]][point_notes[y]]
+
+        multmat = np.log1p(multmat)
         distances *= multmat # Change the idea of distance based on the relationship.
+
         C_distances = np.zeros(distances.shape)
         C_distances[distances > 4.0] = 1
         C_distances[distances < 3.5] = -1
 
-        x_motion = C_distances.ravel() * x_differences * 0.0001
-        y_motion = C_distances.ravel() * y_differences * 0.0001
+        x_motion = C_distances.ravel() * x_differences * 0.002
+        y_motion = C_distances.ravel() * y_differences * 0.002
 
         x_motion = np.sum(np.reshape(x_motion, (N,N)), axis=1) # 0 was interesting anyway.
         y_motion = np.sum(np.reshape(y_motion, (N,N)), axis=1) # 0 was interesting anyway.
 
-        points[:,0] += x_motion + (np.random.random(x_motion.shape)-0.5)*0.1
-        points[:,1] += y_motion + (np.random.random(y_motion.shape)-0.5)*0.1
+        points[:,0] += x_motion + (np.random.random(x_motion.shape)-0.5)*0.01
+        points[:,1] += y_motion + (np.random.random(y_motion.shape)-0.5)*0.01
 
         points[:, 0] -= np.median(points[:, 0])
         points[:, 1] -= np.median(points[:, 1])
 
         #print(trial, x_motion[0])
 
-N = 300
-NOTES = 8
-
-# TODO: Fill this in!
-# It could be cool to have it change dynamically as you play.
-# Then you'd see how different people's music spaces looked.
-# Bigger values mean they should be further apart.
-note_matrix = \
-    [
-        #   C   D   E   F   G   A   B   C
-        [    1,  2,   0.5,   1,   0.5,   1,   0.5,   1.0   ], # C
-        [    0,  1,   2,     1,   0.5,   0.5, 1,     0.5   ], # D
-        [    0,  0,   1,     3,   0.5,   0.5, 0.5,     0.5   ], # E
-        [    0,  0,   0,     1,   1,   0.5, 3,     0.5   ], # F
-        [    0,  0,   0,     0,   1,   1, 0.5,     0.5   ], # G
-        [    0,  0,   0,     0,   0,   1,  2,     1   ], # A
-        [    0,  0,   0,     0,   0,   0, 1,     3   ], # B
-        [    0,  0,   0,     0,   0,   0, 0,     1   ], # C
-    ]
-
-
-# Now make it symmetric.
-for x in range(NOTES):
-    for y in range(x, NOTES):
-        note_matrix[y][x] = note_matrix[x][y]
-
-print(note_matrix)
-
-#note_matrix = np.ones((NOTES, NOTES))
-
-points = np.random.random((N, 2))
-#notes = (np.random.random(N)*(NOTES-1)).astype('int') # Each point has a note.
-notes = np.asarray([q%NOTES for q in range(N)])
-colormap = cm.get_cmap('rainbow', NOTES)
-colors = [(255 * np.asarray(colormap(note)[:-1])).astype('int') for note in notes]
-
-screen = pygame.display.set_mode((500, 500))
-p_clock = pygame.time.Clock()
-
-import time
-#time.sleep(5)
-
 threading.Thread(target=sim).start()
 
-scale = 20
 while True:
-    pygame.draw.rect(screen, (0,0,0), (0,0,500,500))
+    pygame.draw.rect(screen, (0,0,0), (0,0,800,800))
+
+    if i.poll():
+        midi_events = i.read(10)
+        midi_evs = pygame.midi.midis2events(midi_events, i.device_id)
+
+        for e in midi_evs:
+            if e.data2 > 0:
+                player.note_on(e.data1, 127)
+                notes[e.data1] = True
+            if e.data2 <= 0:
+                player.note_off(e.data1, 127)
+                notes[e.data1] = False
+
+            if e.data2 > 0 and e.data1 in [36, 38]:
+                if e.data1 == 36:
+                    current_instrument -= 1
+                    #print(current_instrument)
+                    #player.set_instrument(current_instrument)  # 45
+                if e.data1 == 38:
+                    print(current_instrument)
+                    #current_instrument += 1
+                    #player.set_instrument(current_instrument)  # 45
+
+    for n, note in enumerate(notes):
+        pygame.draw.rect(screen, colors[n%12] if note else (30,30,30), (n*8, 0, 6,50), 0)
+
+    # There's a lot of different ways to model the transition matrix, but it all comes down to what looks cool.
+    # Now draw the transition matrix!
+    for n, note_1 in enumerate(notes):
+        for m, note_2 in enumerate(notes):
+            if note_1 and note_2:
+             #transmat[n%12][m%12] = (transmat[n%12][m%12] + 0.1) * 1.01
+                #transmat[n%12][m%12] = min(1, transmat[n%12][m%12] + 0.04)
+                transmat[n%12][m%12] += 0.5# min(1, transmat[n%12][m%12] + 0.04)
+
+    max_t = np.max(transmat) + 0.1
+    block = 10
+    for x in range(12):
+        for y in range(12):
+            color = alt_colormap(float(transmat[x%12][y%12]/max_t))
+            color = (np.asarray(color[:-1])*255).astype('int')
+            pygame.draw.rect(screen, color, (60 + x*block, 60 + y*block, block-1, block-1), 0)
+
+    lights = [False for _ in range(12)]
+    for n in range(len(notes)):
+        if notes[n]: lights[n%12] = True
 
     for pc in range(len(points)):
         p = points[pc]
-        pygame.draw.rect(screen, colors[pc], (250+ int(scale*p[0]), 250 + int(scale*p[1]), 4, 4), 0)
+        col = colors[pc%12] if lights[pc%12] else (80,80,80) #[int(n/5) for n in colors[pc%12]] # (60, 60, 60)
+        pygame.draw.rect(screen, col, (300+ int(40*p[0]), 250 + int(40*p[1]), 4, 4), 0)
 
+    transmat /= 1.05
     pygame.display.flip()
-    p_clock.tick(30)
+    p_clock.tick(60)
